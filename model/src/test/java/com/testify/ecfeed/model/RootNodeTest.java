0d184586f67ee2e55f54195c80a18480bd03f55d
p.chamuczynski@radytek.com <p.chamuczynski@radytek.com>
2013-11-22 15:47:06 +0100

    unit tests for RootNode

--- model/src/test/java/com/testify/ecfeed/model/RootNodeTest.java
+++ model/src/test/java/com/testify/ecfeed/model/RootNodeTest.java
@@ -18,45 +18,45 @@ import org.junit.Test;
 import com.testify.ecfeed.model.ClassNode;
 import com.testify.ecfeed.model.RootNode;
 
-public class RootNodeTest extends RootNode {
-
-	public RootNodeTest() {
-		super("RootNodeTest");
-	}
-
-	@Test
-	public void test() {
-		RootNode root = new RootNode("root");
-		ClassNode klass = new ClassNode("com.test.ClassName");
-		assertEquals(false, root.hasChildren());
-		assertEquals(0, root.getChildren().size());
-		assertEquals(0, root.getClasses().size());
-		assertEquals(null, klass.getParent());
-
-		root.addClass(klass);
-		assertEquals(true, root.hasChildren());
-		assertEquals(1, root.getChildren().size());
-		assertEquals(1, root.getClasses().size());
-		assertEquals(root, klass.getParent());
-		assertEquals(klass, root.getChildren().get(0));
-	}
+public class RootNodeTest{
 	
 	@Test
-	public void testEquals(){
-		RootNode root = new RootNode("root");
-		ClassNode cl1 = new ClassNode("com.test.Class1");
-		ClassNode cl2 = new ClassNode("com.test.Class2");
-		root.addClass(cl1);
-		root.addClass(cl2);
-
-		RootNode rootCopy = new RootNode("root");
-		ClassNode cl1Copy = new ClassNode("com.test.Class1");
-		ClassNode cl2Copy = new ClassNode("com.test.Class2");
-		rootCopy.addClass(cl1Copy);
-		rootCopy.addClass(cl2Copy);
+	public void tesGetClass(){
+		RootNode root = new RootNode("name");
+		ClassNode classNode1 = new ClassNode("name");
+		ClassNode classNode2 = new ClassNode("name");
+		assertEquals(0,  root.getClasses().size());
+		assertEquals(0,  root.getChildren().size());
+		root.addClass(classNode1);
+		root.addClass(classNode2);
+		assertEquals(2,  root.getClasses().size());
+		assertEquals(2,  root.getChildren().size());
+		assertTrue(root.getClasses().contains(classNode1));
+		assertTrue(root.getChildren().contains(classNode1));
+		assertTrue(root.getClasses().contains(classNode2));
+		assertTrue(root.getChildren().contains(classNode2));
 		
-		assertTrue(root.equals(rootCopy));
-		cl2Copy.setName("com.test.Class2Copy");
-		assertFalse(root.equals(rootCopy));
+		root.removeClass(classNode1);
+		assertEquals(1,  root.getClasses().size());
+		assertEquals(1,  root.getChildren().size());
+		assertFalse(root.getClasses().contains(classNode1));
+		assertFalse(root.getChildren().contains(classNode1));
+		assertTrue(root.getClasses().contains(classNode2));
+		assertTrue(root.getChildren().contains(classNode2));
+	}
+	
+
+	@Test
+	public void testGetClassModel(){
+		RootNode root = new RootNode("name");
+		ClassNode class1 = new ClassNode("com.example.class1");
+		ClassNode class2 = new ClassNode("com.example.class2");
+		ClassNode class3 = new ClassNode("class1");
+		
+		root.addClass(class1);
+		root.addClass(class2);
+		root.addClass(class3);
+		
+		assertEquals(class1, root.getClassModel("com.example.class1"));
 	}
 }
