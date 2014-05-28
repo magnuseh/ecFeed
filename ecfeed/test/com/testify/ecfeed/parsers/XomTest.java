266aa1dfcafb73f0c136461f6cc1a57f2dee8dd6
p.chamuczynski@radytek.com <p.chamuczynski@radytek.com>
2014-05-28 13:28:02 +0200

    initial commit with unit tests

---
+++ ecfeed/test/com/testify/ecfeed/parsers/XomTest.java
@@ -0,0 +1,11 @@
+package com.testify.ecfeed.parsers;
+
+import org.junit.Test;
+
+import com.testify.ecfeed.model.PartitionNode;
+
+public class XomTest {
+	@Test
+	public void partitionTest(){
+	}
+}
